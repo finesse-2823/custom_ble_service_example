d79ccc3d5b926504f4b3ec676a085014954cf9d5
finesse-2309 <puruaniket2309@gmail.com>
2023-05-20 13:50:36 +0530

    initial commits

---
+++ pca10040/s132/ses/Output/Debug/Obj/ble_app_template_pca10040_s132/sensorsim.d
@@ -0,0 +1,9 @@
+Output/Debug/Obj/ble_app_template_pca10040_s132/sensorsim.o: \
+ C:\nRF5_SDK_17.1.0_ddde560\components\libraries\sensorsim\sensorsim.c \
+ C:\nRF5_SDK_17.1.0_ddde560\components\libraries\sensorsim\sensorsim.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.12a/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.12a/include/__SEGGER_RTL.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.12a/include/__SEGGER_RTL_ConfDefaults.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.12a/include/__SEGGER_RTL_Conf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.12a/include/__SEGGER_RTL_Arm_Conf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 7.12a/include/stdbool.h
